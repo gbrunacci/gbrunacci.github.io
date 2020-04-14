9d00bf98a849f4984e9031e5ae84dc498b1ec8fc
Gabriel Brunacci <gbrunacci@gmail.com>
2020-04-14 20:10:46 -0300

    Fixed typos

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -22,7 +22,7 @@ function Home(): JSX.Element {
         </ImageContainer>
         <div>
           <h2>A decade and a half of code.</h2>
-          <p>I started my professional carrer back in 2005, dig into web development in 2007 and went through a lot of buzzwords.</p>
+          <p>I started my professional carrer back in 2005, dug into web development in 2007 and went through a lot of buzzwords.</p>
           <p>
             Currently, my latest skillset includes <Bold>React, React Native, Laravel, and AWS</Bold>. Industry usually considers that a combination of <Bold>Fullstack and DevOps</Bold>.
           </p>
@@ -37,7 +37,7 @@ function Home(): JSX.Element {
         <div>
           <h2>A tech-minded entepreneur.</h2>
           <p>Or the way I like seeing it: I found nice people along the way to work with and build interesting stuff, doing what we love.</p>
-          <p>As part this journey had the luck to work with and for people all around the world since 2010. We learn, fail and succeeded in many ways. And most important, enjoying it.</p>
+          <p>As part this journey had the luck to work with and for people all around the world since 2010. We learned, failed and succeeded in many ways. And most important, enjoying it.</p>
         </div>
         <ImageContainer>
           <BlockImage src={RocketImage} alt="Rocket" />
@@ -52,7 +52,7 @@ function Home(): JSX.Element {
           <p>We would not achieve anything just by ourselves, we grow only when people around us do it was well.</p>
           <p>
             For over six years I have been teaching Databases I in schools, and a bit more coaching trainees and juniors in working environments. I like thinking I got a great deal of soft skills
-            though that.
+            through that.
           </p>
         </div>
       </AboutMeBlock>
