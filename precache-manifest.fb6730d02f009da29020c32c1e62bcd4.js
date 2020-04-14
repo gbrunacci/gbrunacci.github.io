ff940a3cc83de55626d9f06b762327b431b350bd
Gabriel Brunacci <gbrunacci@gmail.com>
2020-04-14 19:06:11 -0300

    Updates

---
+++ precache-manifest.fb6730d02f009da29020c32c1e62bcd4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bd7e0856ffa7137c0d9ca419f53c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "902366e0dc5c944df2da",
+    "url": "/static/css/main.4a149272.chunk.css"
+  },
+  {
+    "revision": "3fa5c096f97dc2379257",
+    "url": "/static/js/2.76c0713c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76c0713c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902366e0dc5c944df2da",
+    "url": "/static/js/main.55514a3f.chunk.js"
+  },
+  {
+    "revision": "02d220b3627fe642e217",
+    "url": "/static/js/runtime-main.b99443d6.js"
+  },
+  {
+    "revision": "df9d1a62c450015f6f294756d7589778",
+    "url": "/static/media/amazonaws.df9d1a62.svg"
+  },
+  {
+    "revision": "e47bde8a99ff1a17813492f0f68b13c2",
+    "url": "/static/media/brain.e47bde8a.svg"
+  },
+  {
+    "revision": "7019b9b893b44f25d14bf9cb86a7b442",
+    "url": "/static/media/github.7019b9b8.svg"
+  },
+  {
+    "revision": "a798374facac0d72734a52301d340a2b",
+    "url": "/static/media/hero.a798374f.jpg"
+  },
+  {
+    "revision": "56729c664dbaead22ea351a50783a246",
+    "url": "/static/media/laravel.56729c66.svg"
+  },
+  {
+    "revision": "7bea0cf8a31d44948fbd0093baab370d",
+    "url": "/static/media/linkedin.7bea0cf8.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "475c6737df6d59eb91a1d2b1363c4803",
+    "url": "/static/media/rocket.475c6737.svg"
+  }
+]);
--- src/components/Bold/index.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Bold = styled.strong`
-  color: rgba(0, 0, 0, 0.8);
-`;
-
-export default Bold;
